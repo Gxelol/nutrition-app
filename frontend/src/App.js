4cb5afcf14685a9fa7def8255250334560c874e0
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2025-09-22 22:50:21 -0300

    Add default react App

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ChatBox from "./components/ChatBox";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Chatbot de Nutrição</h1>
+      {/* <ChatBox /> */}
+    </div>
+  );
+}
+
+export default App;
