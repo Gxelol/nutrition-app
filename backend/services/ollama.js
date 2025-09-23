f728ebf885bc5062f026d77906de51692ebebd5d
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2025-09-22 22:47:01 -0300

    Add function to fetch LLM's message

--- backend/services/ollama.js
+++ backend/services/ollama.js
@@ -6,7 +6,7 @@ export async function askOllama(message) {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
-      model: "llama3",   // Pode trocar para outro modelo
+      model: "llama3",
       prompt: message
     })
   });
