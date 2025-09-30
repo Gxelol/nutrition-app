322a04a75e6074fbf407681d194c91615f831013
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2025-09-29 23:08:53 -0300

    feat: reseting page to new functionality

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,9 @@
-export default function Home() {
+export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">Welcome to the Nutrition Chatbot!</h1>
+    <main className="flex flex-col items-center justify-center h-screen text-center">
+      <div>
+        <h1>Olá mundo!</h1>
+      </div>
     </main>
   );
 }
