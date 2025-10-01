840cbfc43d03cdfd835af1bea16a7cb3787ede2c
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2025-09-30 21:12:27 -0300

    chore: delete unnecessary filles

--- frontend/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function HomePage() {
-  return (
-    <main className="flex flex-col items-center justify-center h-screen text-center">
-      <div>
-        <h1>Olá mundo!</h1>
-      </div>
-    </main>
-  );
-}
