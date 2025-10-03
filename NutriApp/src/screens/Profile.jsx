0d3e406543fe275c1644485cfe285ef0a6fedf46
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2025-10-03 18:23:57 -0300

    feat: add new screens

---
+++ NutriApp/src/screens/Profile.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from '../styles/Profile.styles';
+import { View, Text, ScrollView } from 'react-native';
+
+export default function ProfileScreen() {
+  return (
+   
+  );
+}
