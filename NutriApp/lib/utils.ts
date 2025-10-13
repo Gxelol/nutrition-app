61b2c81dd2485436bffe7d6d69d9f32a964f6a80
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2025-10-12 22:07:35 -0300

    chore: add dependencies and first configs

---
+++ NutriApp/lib/utils.ts
@@ -0,0 +1,7 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+// This function is used to merge class names conditionally and handle Tailwind CSS conflicts.
