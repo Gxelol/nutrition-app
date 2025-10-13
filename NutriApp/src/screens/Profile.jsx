1c50f2784affe14a4460ddc3deafc390b8f3d848
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2025-10-12 22:26:30 -0300

    chore: add dependencies and configs

--- NutriApp/src/screens/Profile.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import styles from '../styles/Profile.styles';
-import { View, Text, ScrollView } from 'react-native';
-
-export default function ProfileScreen() {
-  return (
-   
-  );
-}
--- NutriApp/src/screens/Settings.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import styles from '../styles/Settings.styles';
-import { View, Text, ScrollView } from 'react-native';
-
-export default function SettingsScreen() {
-  return (
-   
-  );
-}
